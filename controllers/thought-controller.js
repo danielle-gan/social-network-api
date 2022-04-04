9da21210e3c4992b9e873ef8b333449b5f93740f
danielle-gan <danielle.gan@uconn.edu>
2022-04-03 23:05:14 -0400

    working on thought controller again

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -7,14 +7,17 @@ const thoughtController = {
                 path: 'reactions',
                 select: '-__v'
             })
-            .populate({
-                path: 'thoughtText',
-                select: '-__v',
-            })
+            // .populate({
+            //     path: 'thoughtText',
+            //     select: '-__v',
+            // })
             .select('-__v')
             .sort({ _id: -1 })
             .then(dbData => res.json(dbData))
-            .catch(err => { res.status(400).json(err) });
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err)
+            });
     },
 
     createThought({ body }, res) {
